56c5d953dc268d7b1066a5ba09afd9c3ce33ff92
Lishavadivelmurgan <LISHAMURGAN2106@GMAIL.COM>
2025-07-31 22:22:26 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNZ
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            PNZ n = new PNZ(num);
+            n.check();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PNZ
+{
+    public class PNZ
+
+    {
+        int num;
+        public PNZ()
+        {
+            num = 2;
+        }
+        public PNZ(int num)
+        {
+            this.num = num;
+        }
+        public void check()
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("the number is zero.");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("the number is positive.");
+            }
+            else
+            {
+                Console.WriteLine("the number is negative.");
+            }
+        }
+    }
+}
+                
